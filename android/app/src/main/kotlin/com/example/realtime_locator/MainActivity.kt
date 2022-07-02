b26e74132578259e92b13e53ca4808163bb14e8d
kz422 <p.inoue.p@gmail.com>
2022-07-02 17:26:58 +0900

    ignore

--- android/app/src/main/kotlin/com/example/realtime_locator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.realtime_locator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-const String google_api_key = 'AIzaSyAzgotrlnDECB-BxTPjEUOXmeSxbt9VPzM';
